99a1de0d00b88e41afc6421d4ce24cf64fbf076e
i7E-025 <minsu4867@naver.com>
2023-12-14 16:57:34 +0900

    modify page_present(1214).html

--- public/js/page_present.js
+++ public/js/page_present.js
@@ -96,4 +96,49 @@ window.onload = function() {
         }
     });//ajax
 
+    $.ajax({
+        type: 'get',
+        url: '../list_3.json',
+        dataType: 'json',
+        success: function(data) {
+            console.log(data);
+
+            data.forEach((list, i) => {
+                $(".goods_list_3").append(
+                    `<li class="gl_item d-flex">
+                        <div class="gl_inner_item_wrap">
+                        <!-- 상품 이미지 -->
+                            <div class="gl_image">
+                                <a href="/page_present_detail">
+                                    <img src="${list.flowerImg}" alt="1">
+                                </a>
+                            </div>
+                            <div class="resp_display_goods_info infO_style_1">
+                                <!-- 상품명 -->
+                                <div class="goods_info display_goods_name">
+                                    <span>
+                                        <a href="#">${list.flowerTitle}</a>
+                                    </span>
+                                </div>
+                                <div class="info_group">
+                                    <!-- 정가 -->
+                                    <div class="goodS_info display_sales_price">
+                                        <span class="area">
+                                            <span class="won">${list.flowerPrice.toLocaleString()}</span>
+                                            원
+                                        </span>
+                                    </div>
+                                    <!-- 할인율 -->
+                                    <div class="goods_evaluate">${list.flowerEvaluate}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>`
+                );
+            });
+        }
+    });//ajax
+
+    
+
 };//javascript
